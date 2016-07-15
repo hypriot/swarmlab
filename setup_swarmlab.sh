e799022de5a79dda2a56eff6c952fdae1be7e369
Govinda Fichtner <govinda.fichtner@paessler.com>
2016-07-15 15:00:55 +0200

    refactor and cleanup

--- setup_swarmlab.sh
+++ setup_swarmlab.sh
@@ -3,7 +3,8 @@
 # install Docker and dependencies
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-wily testing" > /etc/apt/sources.list.d/docker.list
-apt-get update
-apt-get install -y linux-image-extra-"$(uname -r)"
 
+apt-get update
+
+apt-get install -y linux-image-extra-"$(uname -r)"
 apt-get install -y docker-engine=1.12.0~rc3-0~wily
